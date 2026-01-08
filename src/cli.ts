3e1ffc6443462e97cbc35a52522ca51fec49a2f9
francis <dominiquefrancis42@gmail.com>
2026-01-08 22:06:01 +0800

    refactor: remove folders and make files intead for services

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,8 @@
-console.log("workingg")
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .name("devtask")
+  .description("A CLI task management tool for developers that organizes work")
+  .version("1.0.0");
