dbf66c1c087ed758cef0bdd98fe3c9b6f36a5b59
francis <dominiquefrancis42@gmail.com>
2026-01-20 09:09:04 +0800

    refactor(features): optimize queries

--- src/db/queries/features.ts
+++ src/db/queries/features.ts
@@ -1,156 +1,142 @@
 import type { Feature, FeatureRunResult } from "../../types/Features.js";
 import { db } from "../database.js";
 
-export const getAllFeatures = (projectId: number): Feature[] => {
-  return db
-    .prepare<[number], Feature>("SELECT * from features WHERE project_id = ?")
-    .all(projectId);
+const stmts = {
+  getAllFeatures: db.prepare<[number], Feature>(
+    "SELECT * from features WHERE project_id = ?",
+  ),
+  getFeature: db.prepare<[number], Feature>(
+    "SELECT * from features WHERE id = ?",
+  ),
+  getAllUnfocus: db.prepare<[number], Feature>(
+    "SELECT * from features WHERE project_id = ? and  is_focused = 0",
+  ),
+  getAllFocus: db.prepare<[number], Feature>(
+    "SELECT * from features WHERE project_id = ? and  is_focused = 1",
+  ),
+  insert: db.prepare<[string, number], Feature>(
+    "INSERT INTO features (description, project_id) VALUES (?, ?)",
+  ),
+  batchInsert: db.prepare<[string, number], Feature>(
+    "INSERT INTO features (description, project_id) VALUES (?, ?)",
+  ),
+  insertNotes: db.prepare<[string, number, number], FeatureRunResult>(
+    "UPDATE features SET notes = ? WHERE id = ? AND project_id = ?",
+  ),
+  focus: db.prepare<[number, number], FeatureRunResult>(
+    "UPDATE features SET is_focused = 1, status = 'in_progress' WHERE id = ? AND project_id = ?",
+  ),
+  focusMultiple: db.prepare(
+    "UPDATE features SET is_focused = 1, status = 'in_progress' WHERE id = ?",
+  ),
+  unfocus: db.prepare(
+    "UPDATE features SET is_focused = 0, status = 'todo' WHERE id = ?",
+  ),
+  setStatus: db.prepare<[number, number], FeatureRunResult>(
+    "UPDATE features SET is_focused = 0, status = 'done' WHERE id = ? AND project_id = ?",
+  ),
+  delete: db.prepare("DELETE FROM features WHERE id = ?"),
 };
 
+// ============
+// GET QUERIES
+// ============
+export const getAllFeatures = (projectId: number): Feature[] =>
+  stmts.getAllFeatures.all(projectId);
+
+export const getFeature = (featId: number): Feature | undefined =>
+  stmts.getFeature.get(featId);
+
+export const getAllUnfocusedFeatures = (projectId: number): Feature[] =>
+  stmts.getAllUnfocus.all(projectId);
+
+export const getAllFocusedFeatures = (projectId: number): Feature[] =>
+  stmts.getAllFocus.all(projectId);
+
+// ===============
+// INSERT QUERIES
+// ===============
 export const insertFeature = (
   description: string,
   projectId: number,
 ): FeatureRunResult => {
-  const result = db
-    .prepare<
-      [string, number],
-      Feature
-    >("INSERT INTO features (description, project_id) VALUES (?, ?)")
-    .run(description, projectId);
+  const result = stmts.insert.run(description, projectId);
   return { changes: result.changes, lastInsertRowid: result.lastInsertRowid };
 };
+const performInsert = db.transaction((desc: string[], id: number) => {
+  let changes = 0;
+  let lastInsertRowid: number | bigint = 0;
+  for (const d of desc) {
+    const feature = stmts.batchInsert.run(d, id);
+    changes += feature.changes;
+    lastInsertRowid = feature.lastInsertRowid;
+  }
+  return { changes, lastInsertRowid };
+});
 
 export const batchInsert = (
   description: string[],
   projectId: number,
-): FeatureRunResult => {
-  const performInsert = db.transaction((desc: string[], id: number) => {
-    let changes = 0;
-    let lastInsertRowid: number | bigint = 0;
-    for (const d of desc) {
-      const feature = db
-        .prepare<
-          [string, number],
-          Feature
-        >("INSERT INTO features (description, project_id) VALUES (?, ?)")
-        .run(d, id);
-      changes += feature.changes;
-      lastInsertRowid = feature.lastInsertRowid;
-    }
-    return { changes, lastInsertRowid };
-  });
-  return performInsert(description, projectId);
-};
-
-export const getFeature = (featId: number): Feature | undefined => {
-  return db
-    .prepare<[number], Feature>("SELECT * from features WHERE id = ?")
-    .get(featId);
-};
-
-export const setFocus = (
-  featId: number,
-  projectId: number,
-): FeatureRunResult => {
-  const result = db
-    .prepare<
-      [number, number],
-      FeatureRunResult
-    >("UPDATE features SET is_focused = 1, status = 'in_progress' WHERE id = ? AND project_id = ?")
-    .run(featId, projectId);
-
-  return result;
-};
-
-export const getAllUnfocusedFeatures = (projectId: number): Feature[] => {
-  return db
-    .prepare<
-      [number],
-      Feature
-    >("SELECT * from features WHERE project_id = ? and  is_focused = 0")
-    .all(projectId);
-};
-
-export const getAllFocusedFeatures = (projectId: number): Feature[] => {
-  return db
-    .prepare<
-      [number],
-      Feature
-    >("SELECT * from features WHERE project_id = ? and  is_focused = 1")
-    .all(projectId);
-};
-
-export const setMultipleFocus = (featId: number[]): FeatureRunResult => {
-  const performAction = db.transaction((ids: number[]) => {
-    let changes = 0;
-    let lastInsertRowid: bigint | number = 0;
-    for (const id of ids) {
-      const result = db
-        .prepare(
-          "UPDATE features SET is_focused = 1, status = 'in_progress' WHERE id = ?",
-        )
-        .run(id);
-      changes += result.changes;
-      lastInsertRowid = result.lastInsertRowid;
-    }
-    return { changes, lastInsertRowid };
-  });
-  return performAction(featId);
-};
-
-export const setUnfocus = (featId: number[]): FeatureRunResult => {
-  const performAction = db.transaction((ids: number[]) => {
-    let changes = 0;
-    let lastInsertRowid: bigint | number = 0;
-    for (const id of ids) {
-      const result = db
-        .prepare(
-          "UPDATE features SET is_focused = 0, status = 'todo' WHERE id = ?",
-        )
-        .run(id);
-      changes += result.changes;
-      lastInsertRowid = result.lastInsertRowid;
-    }
-    return { changes, lastInsertRowid };
-  });
-  return performAction(featId);
-};
-
-export const setStatusDone = (
-  featId: number,
-  projectId: number,
-): FeatureRunResult => {
-  return db
-    .prepare<
-      [number, number],
-      FeatureRunResult
-    >("UPDATE features SET is_focused = 0, status = 'done' WHERE id = ? AND project_id = ?")
-    .run(featId, projectId);
-};
-
-export const deleteFeat = (featId: number[]): FeatureRunResult => {
-  const performAction = db.transaction((ids: number[]) => {
-    let changes = 0;
-    let lastInsertRowid: bigint | number = 0;
-    for (const id of ids) {
-      const result = db.prepare("DELETE FROM features WHERE id = ?").run(id);
-      changes += result.changes;
-      lastInsertRowid = result.lastInsertRowid;
-    }
-    return { changes, lastInsertRowid };
-  });
-  return performAction(featId);
-};
+): FeatureRunResult => performInsert(description, projectId);
 
 export const insertNotes = (
   notes: string,
   featId: number,
   projectId: number,
-): FeatureRunResult => {
-  return db
-    .prepare<
-      [string, number, number],
-      FeatureRunResult
-    >("UPDATE features SET notes = ? WHERE id = ? AND project_id = ?")
-    .run(notes, featId, projectId);
-};
+): FeatureRunResult => stmts.insertNotes.run(notes, featId, projectId);
+
+// ==============
+// UPDATE QUERIES
+// ==============
+export const setFocus = (featId: number, projectId: number): FeatureRunResult =>
+  stmts.focus.run(featId, projectId);
+
+const performFocus = db.transaction((ids: number[]) => {
+  let changes = 0;
+  let lastInsertRowid: bigint | number = 0;
+  for (const id of ids) {
+    const result = stmts.focusMultiple.run(id);
+    changes += result.changes;
+    lastInsertRowid = result.lastInsertRowid;
+  }
+  return { changes, lastInsertRowid };
+});
+
+export const setMultipleFocus = (featId: number[]): FeatureRunResult =>
+  performFocus(featId);
+
+const performUnfocus = db.transaction((ids: number[]) => {
+  let changes = 0;
+  let lastInsertRowid: bigint | number = 0;
+  for (const id of ids) {
+    const result = stmts.unfocus.run(id);
+    changes += result.changes;
+    lastInsertRowid = result.lastInsertRowid;
+  }
+  return { changes, lastInsertRowid };
+});
+
+export const setUnfocus = (featId: number[]): FeatureRunResult =>
+  performUnfocus(featId);
+
+export const setStatusDone = (
+  featId: number,
+  projectId: number,
+): FeatureRunResult => stmts.setStatus.run(featId, projectId);
+
+// =============
+// DELETE QUERY
+// =============
+const performDelete = db.transaction((ids: number[]) => {
+  let changes = 0;
+  let lastInsertRowid: bigint | number = 0;
+  for (const id of ids) {
+    const result = stmts.delete.run(id);
+    changes += result.changes;
+    lastInsertRowid = result.lastInsertRowid;
+  }
+  return { changes, lastInsertRowid };
+});
+
+export const deleteFeat = (featId: number[]): FeatureRunResult =>
+  performDelete(featId);
