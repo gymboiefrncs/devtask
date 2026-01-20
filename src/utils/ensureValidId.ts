5d77a3b0cfc4993b263897be5a8a806e1e0d7b4a
francis <dominiquefrancis42@gmail.com>
2026-01-20 10:32:59 +0800

    fix log message

--- src/utils/ensureValidId.ts
+++ src/utils/ensureValidId.ts
@@ -2,7 +2,7 @@ export const ensureValidId = (projectId: string | number): number | Error => {
   if (typeof projectId === "number") return projectId;
 
   const id = Number(projectId);
-  if (isNaN(id) || id <= 0) return new Error("Invalid project id");
+  if (isNaN(id) || id <= 0) return new Error("Invalid id");
 
   return id;
 };
